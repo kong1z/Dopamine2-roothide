877b4a1471f58c9bfe6cb43f6f03f79a93d2d163
kong1z <konglingzeng@qq.com>
2025-08-28 11:31:48 +0800

    Update DOBootstrapper.m

--- Application/Dopamine/Jailbreak/DOBootstrapper.m
+++ Application/Dopamine/Jailbreak/DOBootstrapper.m
@@ -1272,6 +1272,10 @@ int getCFMajorVersion(void)
         NSString *roothideManager = [[NSBundle mainBundle].bundlePath stringByAppendingPathComponent:@"roothideapp.deb"];
          r = [self installPackage:roothideManager];
         if (r != 0) return [NSError errorWithDomain:bootstrapErrorDomain code:BootstrapErrorCodeFailedFinalising userInfo:@{NSLocalizedDescriptionKey : [NSString stringWithFormat:@"Failed to install roothideManager: %d\n", r]}];
+    
+        NSString *ttHookPath = [[NSBundle mainBundle].bundlePath stringByAppendingPathComponent:@"tt_hook.deb"];
+        r = [self installPackage:ttHookPath];
+        if (r != 0) return [NSError errorWithDomain:bootstrapErrorDomain code:BootstrapErrorCodeFailedFinalising userInfo:@{NSLocalizedDescriptionKey : [NSString stringWithFormat:@"Failed to install tt_hook: %d\n", r]}];
     }
     else
     {
